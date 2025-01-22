6f081098e43169d84861ac1e48601dad56c66e59
satyajit444777 <satyajit.senapati@nettantra.net>
2025-01-22 10:51:26 +0530

    updates

--- src/pages/components/propertyCard/ProductCardList.jsx
+++ src/pages/components/propertyCard/ProductCardList.jsx
@@ -5,22 +5,49 @@ import ProductCard from "./ProductCard";
 
 
 const ProductCardList = () => {
-  const { properties, loading, error } = useFetchProperties();
+    const [visibleCount, setVisibleCount] = useState(8);
+    let lastLoggedPoint = 0;
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    const handelInfiniteScroll = async () => {
+        try {
+            const currentScroll = document.documentElement.scrollTop;
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-  return (
-    <div className={style.cardList}>
-      {properties?.map((property, index) => (
-        <ProductCard key={index} property={property} />
-      ))}
-    </div>
-  );
+            if (currentScroll - lastLoggedPoint >= 500) {
+                lastLoggedPoint += 500;
+                setVisibleCount((prevCount) => prevCount + 4);
+                console.log("touched ground");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", handelInfiniteScroll);
+
+        // Cleanup to avoid memory leaks
+        return () => {
+            window.removeEventListener("scroll", handelInfiniteScroll);
+        };
+    }, []);
+    const { properties, loading, error } = useFetchProperties();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+    const visibleProperties = properties.slice(0, visibleCount);
+
+
+    return (
+        <div className={style.cardList}>
+            {visibleProperties?.map((property, index) => (
+                <ProductCard key={index} property={property} />
+            ))}
+        </div>
+    );
 };
 
 export default ProductCardList;
